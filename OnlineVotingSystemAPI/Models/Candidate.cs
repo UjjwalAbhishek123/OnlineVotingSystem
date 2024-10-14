7feee2c02d422263a889e8f8c57e67624af076b7
UjjwalAbhishek123 <ujjwalabhishek15@gmail.com>
2024-10-14 16:43:15 +0530

    6.1 commit updated annotation in Candidate.cs file

--- OnlineVotingSystemAPI/Models/Candidate.cs
+++ OnlineVotingSystemAPI/Models/Candidate.cs
@@ -14,6 +14,8 @@ namespace OnlineVotingSystemAPI.Models
         [Required(ErrorMessage = "Party name is required.")]
         [StringLength(100, ErrorMessage = "Party Name can't be longer than 100 characters.")]
         public string Party { get; set; }
+
+        [Required]
         public int VotingEventId { get; set; } // Foreign key to VotingEvent
         public VotingEvent VotingEvent { get; set; } // Navigation property
     }
